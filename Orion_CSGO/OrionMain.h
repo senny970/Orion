4e3d920e4e577d55c589ee81530792e67a4a172a
Astarion <bebopvox@yandex.ru>
2017-11-16 04:59:06 +0300

    Orion instant hooks for Counter Strike Global Offensive by Astarion!

---
+++ Orion_CSGO/OrionMain.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/********************************************************************
+
+Orion instant hooks for Counter Strike Global Offensive by Astarion!
+
+*********************************************************************/
+
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
